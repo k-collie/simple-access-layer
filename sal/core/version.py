64c7767fa791244af39079bcfd32e53109945abb
Kingsley Collie <kingsley.collie@ukaea.uk>
2025-01-10 18:32:45 +0000

    De-duplicate package metadata, improve version parsing

--- sal/core/version.py
+++ sal/core/version.py
@@ -1,13 +1,3 @@
-"""
-Parses the package version file and exports it to constant.
+import importlib_metadata
 
-The package version string is stored in the sal/VERSION text file.
-"""
-
-from os import path
-
-# parse the package version number
-with open(path.join(path.dirname(__file__), 'VERSION')) as version_file:
-    VERSION = version_file.read().strip()
-
-
+VERSION = importlib_metadata.version("sal")
